f7dcce2888a8d508331a3fd0c0cd30129282b369
Galushkin Nikita <94059038+Kuller27@users.noreply.github.com>
2024-12-27 17:12:50 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,11 +1,11 @@
 import math
 
 
-def calculate_area(radius):
-    assert radius >= 0
-    return math.pi * radius * radius
+def calculate_area(r):
+    assert r >= 0
+    return math.pi * r * r
 
 
-def calculate_perimeter(radius):
-    assert radius >= 0
-    return 2 * math.pi * radius
+def calculate_perimeter(r):
+    assert r >= 0
+    return 2 * math.pi * r
