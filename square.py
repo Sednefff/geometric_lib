2990d951fffaa04a7a55aee984adfc5f1a91e233
Galushkin Nikita <94059038+Kuller27@users.noreply.github.com>
2024-12-27 16:43:20 +0300

    Update square.py
    rework code

--- square.py
+++ square.py
@@ -1,7 +1,8 @@
-def area(a):
-    return a * a
+def area(side):
+    assert side >= 0
+    return side * side
 
 
-def perimeter(a):
-    return 4 * a
-
+def perimeter(side):
+    assert side >= 0
+    return 4 * side
