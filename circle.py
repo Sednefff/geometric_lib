8941bd7cb6c4d0a44e35df85f6b2f7bd2384cb00
sssssspecial <yanetoper28381@gmail.com>
2025-03-04 22:48:06 +0300

    added documentation to files

--- circle.py
+++ circle.py
@@ -3,8 +3,8 @@ import math
 
 def area(r):
     return math.pi * r * r
-
+'''math.pi = число пи, r=радиус круга, чью площадь нам надо посчитать'''
 
 def perimeter(r):
     return 2 * math.pi * r
-
+''' math.pi-аналогично, r-радиус, функция возвращает периметр круга'''
