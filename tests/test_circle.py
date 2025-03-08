6868cfdc85baae04ca4998e8d4477e2656cf0b24
bobby fischer <yanetoper28381@icloud.com>
2025-03-08 17:31:51 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -7,16 +7,6 @@ def test_area():
     assert area(2) == pytest.approx(12.56637, 0.0001)
 
 
-def test_area_invalid():
-    with pytest.raises(TypeError):
-        area("Not a number.")
-
-
 def test_perimeter():
     assert perimeter(1) == pytest.approx(6.28318, 0.0001)
     assert perimeter(2) == pytest.approx(12.56637, 0.0001)
-
-
-def test_perimeter_invalid():
-    with pytest.raises(TypeError):
-        perimeter("Not a number.")
