c8d2bc01f57e7365ffa9a24ce8a419989a07f327
bobby fischer <yanetoper28381@icloud.com>
2025-03-08 17:40:42 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -5,6 +5,7 @@ def area(r):
     return math.pi * r * r
 '''math.pi = число пи, r=радиус круга, чью площадь нам надо посчитать'''
 
+
 def perimeter(r):
     return 2 * math.pi * r
 ''' math.pi-аналогично, r-радиус, функция возвращает периметр круга'''
