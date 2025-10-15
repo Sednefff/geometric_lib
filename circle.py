87aeecb0c6e73356aa4ca4cff3de382b1f11394e
Tarklez <tarklez.dev@gmail.com>
2025-10-15 22:56:50 +0300

    Documented functions

--- circle.py
+++ circle.py
@@ -2,9 +2,27 @@ import math
 
 
 def area(r):
+    '''
+    Находит площадь круга
+    
+        Параметры:
+        r (float): радиус
+        
+        Возвращаемое значение:
+        area (float): площадь круга
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+    Находит периметр круга
+    
+        Параметры:
+        r (float): радиус
+        
+        Возвращаемое значение:
+        perimeter (float): периметр круга
+    '''
     return 2 * math.pi * r
 
